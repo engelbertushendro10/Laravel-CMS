8e5f262e013221ba179e8eab700429bcbe0b672d
engelbertushendro10 <engelbertushendro@gmail.com>
2021-11-07 04:04:45 -0800

    dashboard routes

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.dashboard')
+
+
+@section('title')
+    halaman utama
+@endsection
+
+
+@section('breadcrumbs')
+    
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h2>selamat datang</h2>
+        </div>
+    </div>
+@endsection
